f759443b8850b0268b805ba0b195d709706012fc
Pedro Jesus Gomez Duran <pedro.duran@printi.com.br>
2023-05-16 12:51:29 -0300

    carton

---
+++ app/carton/page.js
@@ -0,0 +1,91 @@
+export default function Home() {
+    return (
+      <div>
+        <div className="Featuredstyles__Wrapper-sc-1hixi2t-0 hPJqJm">
+          <div className="Featuredstyles__WrapperInfos-sc-1hixi2t-1 fbJGBY">
+            <h1
+              itemProp="name"
+              color="normal"
+              transform="none"
+              className="styles__TextStyle-sc-6i4w51-0 iXwXlO"
+            >
+              Cartão de Visita
+            </h1>
+            <h2
+              itemProp="description"
+              color="normal"
+              transform="none"
+              className="styles__TextStyle-sc-6i4w51-0 gKRbsM"
+            >
+              Cause impacto no primeiro contato
+            </h2>
+            <p
+              color="normal"
+              transform="none"
+              className="styles__TextStyle-sc-6i4w51-0 eoQrbs"
+            >
+              A partir de*
+            </p>
+            <div
+              itemProp="offers"
+              itemScope=""
+              itemType="http://schema.org/Offer"
+            >
+              <link itemProp="availability" href="http://schema.org/InStock" />
+              <p
+                color="primary"
+                transform="none"
+                className="styles__TextStyle-sc-6i4w51-0 cvuDaU"
+              >
+                <span itemProp="priceCurrency" content="BRL">
+                  R${" "}
+                </span>
+                <span
+                  itemProp="price"
+                  content="24.49"
+                >
+                  24,49
+                </span>
+                <span
+                  color="normal"
+                  transform="none"
+                  className="styles__TextStyle-sc-6i4w51-0 eoQrbs"
+                >
+                  / 100 un
+                </span>
+              </p>
+            </div>
+            <button
+              kind="success"
+              data-optimum="product-cta"
+              role="button"
+              type="button"
+              className="styles__ButtonStyle-sc-18xfwxu-0 hmzFiH"
+            >
+              Configure este produto
+            </button>
+            <p
+              color="normal"
+              transform="none"
+              className="styles__TextStyle-sc-6i4w51-0 iIGtzt"
+            >
+              Na próxima tela você pode configurar as especificações do seu
+              produto e conferir o preço
+            </p>
+            <div className="Featuredstyles__WrapperTeaser-sc-1hixi2t-3 kBojsM">
+              <p>
+                Simulação de compra no formato 90x48mm (retangular), impressão
+                4x0, papel Couché Brilho 300g e sem acabamento.
+              </p>
+              <p>
+                <br />
+              </p>
+              <p>*Preço sem frete, consulte no final da configuração</p>
+              <p>** As imagens representadas são meramente ilustrativas</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
--- app/page.js
+++ app/page.js
@@ -1,95 +1,92 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div>
+      <div className="Featuredstyles__Wrapper-sc-1hixi2t-0 hPJqJm">
+        <div className="Featuredstyles__WrapperInfos-sc-1hixi2t-1 fbJGBY">
+          <h1
+            itemProp="name"
+            color="normal"
+            transform="none"
+            className="styles__TextStyle-sc-6i4w51-0 iXwXlO"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Cartão de Visita
+          </h1>
+          <h2
+            itemProp="description"
+            color="normal"
+            transform="none"
+            className="styles__TextStyle-sc-6i4w51-0 gKRbsM"
+          >
+            Cause impacto no primeiro contato
+          </h2>
+          <p
+            color="normal"
+            transform="none"
+            className="styles__TextStyle-sc-6i4w51-0 eoQrbs"
+          >
+            A partir de*
+          </p>
+          <div
+            itemProp="offers"
+            itemScope=""
+            itemType="http://schema.org/Offer"
+          >
+            <link itemProp="availability" href="http://schema.org/InStock" />
+            <p
+              color="primary"
+              transform="none"
+              className="styles__TextStyle-sc-6i4w51-0 cvuDaU"
+            >
+              <span itemProp="priceCurrency" content="BRL">
+                R${" "}
+              </span>
+              <span
+                itemProp="price"
+                content="24.49"
+                style={{ display: "none" }}
+              >
+                24.49
+              </span>
+              <span>24,49</span>
+              <span
+                color="normal"
+                transform="none"
+                className="styles__TextStyle-sc-6i4w51-0 eoQrbs"
+              >
+                / 100 un
+              </span>
+            </p>
+          </div>
+          <button
+            kind="success"
+            data-optimum="product-cta"
+            role="button"
+            type="button"
+            className="styles__ButtonStyle-sc-18xfwxu-0 hmzFiH"
+          >
+            Configure este produto
+          </button>
+          <p
+            color="normal"
+            transform="none"
+            className="styles__TextStyle-sc-6i4w51-0 iIGtzt"
+          >
+            Na próxima tela você pode configurar as especificações do seu
+            produto e conferir o preço
+          </p>
+          <div className="Featuredstyles__WrapperTeaser-sc-1hixi2t-3 kBojsM">
+            <p>
+              Simulação de compra no formato 90x48mm (retangular), impressão
+              4x0, papel Couché Brilho 300g e sem acabamento.
+            </p>
+            <p>
+              <br />
+            </p>
+            <p>*Preço sem frete, consulte no final da configuração</p>
+            <p>** As imagens representadas são meramente ilustrativas</p>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
